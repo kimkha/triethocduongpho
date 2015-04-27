e26bd413eeba2a63f5fb8661c17a96570134c2a2
Nguyễn Kim Kha <nkimkha@gmail.com>
2015-04-27 17:24:36 +0700

    Add network dialog

--- app/src/main/java/com/kimkha/triethocduongpho/MainActivity.java
+++ app/src/main/java/com/kimkha/triethocduongpho/MainActivity.java
@@ -1,17 +1,26 @@
 package com.kimkha.triethocduongpho;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.provider.Settings;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.widget.DrawerLayout;
 import android.widget.Toast;
 
 import com.kimkha.triethocduongpho.data.Category;
+
+import java.net.InetAddress;
 
 
 public class MainActivity extends ActionBarActivity
@@ -28,34 +37,58 @@ public class MainActivity extends ActionBarActivity
     private CharSequence mTitle;
     private Menu optionsMenu;
     private MainFragment mFragment;
+    private boolean isNetworkAvailable = false;
+    private AlertDialog networkDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mNavigationDrawerFragment = (NavigationDrawerFragment)
-                getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
-        mTitle = getTitle();
+        if (displayNetworkAlert()) {
+            isNetworkAvailable = true;
 
-        // Set up the drawer.
-        mNavigationDrawerFragment.setUp(
-                R.id.navigation_drawer,
-                (DrawerLayout) findViewById(R.id.drawer_layout));
+            mNavigationDrawerFragment = (NavigationDrawerFragment)
+                    getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+            mTitle = getTitle();
+
+            // Set up the drawer.
+            mNavigationDrawerFragment.setUp(
+                    R.id.navigation_drawer,
+                    (DrawerLayout) findViewById(R.id.drawer_layout));
+        } else {
+            isNetworkAvailable = false;
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        Log.e("AAA", "isNetworkAvailable " + isNetworkAvailable);
+        if (!isNetworkAvailable && displayNetworkAlert()) {
+            // Old = not connect, New = connected => reload activity
+            Log.e("AAA", "Restart");
+            isNetworkAvailable = true;
+            Intent intent = getIntent();
+            finish();
+            startActivity(intent);
+        }
     }
 
     @Override
     public void onNavigationDrawerItemSelected(int position) {
-        // update the main content by replacing fragments
-        String category = Category.CATEGORY_LIST[position];
-        mFragment = MainFragment.newInstance(category);
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        fragmentManager.beginTransaction()
-                .replace(R.id.container, mFragment)
-                .commit();
+        if (isNetworkAvailable) {
+            // update the main content by replacing fragments
+            String category = Category.CATEGORY_LIST[position];
+            mFragment = MainFragment.newInstance(category);
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            fragmentManager.beginTransaction()
+                    .replace(R.id.container, mFragment)
+                    .commit();
 
-        mTitle = category;
-        restoreActionBar();
+            mTitle = category;
+            restoreActionBar();
+        }
     }
 
     public void restoreActionBar() {
@@ -69,7 +102,7 @@ public class MainActivity extends ActionBarActivity
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         this.optionsMenu = menu;
-        if (!mNavigationDrawerFragment.isDrawerOpen()) {
+        if (isNetworkAvailable && !mNavigationDrawerFragment.isDrawerOpen()) {
             // Only show items in the action bar relevant to this screen
             // if the drawer is not showing. Otherwise, let the drawer
             // decide what to show in the action bar.
@@ -82,19 +115,21 @@ public class MainActivity extends ActionBarActivity
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_refresh:
-                Toast.makeText(this, "Refresh selected", Toast.LENGTH_SHORT)
-                        .show();
-                setRefreshActionButtonState(true);
-                mFragment.loadListFromStart();
-                break;
-            case R.id.action_settings:
-                Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT)
-                        .show();
-                break;
-            default:
-                break;
+        if (isNetworkAvailable) {
+            switch (item.getItemId()) {
+                case R.id.action_refresh:
+                    Toast.makeText(this, "Refresh selected", Toast.LENGTH_SHORT)
+                            .show();
+                    setRefreshActionButtonState(true);
+                    mFragment.loadListFromStart();
+                    break;
+                case R.id.action_settings:
+                    Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT)
+                            .show();
+                    break;
+                default:
+                    break;
+            }
         }
 
         return super.onOptionsItemSelected(item);
@@ -102,11 +137,13 @@ public class MainActivity extends ActionBarActivity
 
     @Override
     public void onItemSelected(Long id, String title, String imgUrl) {
-        Intent pageIntent = new Intent(this, PageActivity.class);
-        pageIntent.putExtra(PageFragment.ARG_ITEM_ID, id);
-        pageIntent.putExtra(PageFragment.ARG_ITEM_TITLE, title);
-        pageIntent.putExtra(PageFragment.ARG_ITEM_IMG, imgUrl);
-        startActivity(pageIntent);
+        if (isNetworkAvailable) {
+            Intent pageIntent = new Intent(this, PageActivity.class);
+            pageIntent.putExtra(PageFragment.ARG_ITEM_ID, id);
+            pageIntent.putExtra(PageFragment.ARG_ITEM_TITLE, title);
+            pageIntent.putExtra(PageFragment.ARG_ITEM_IMG, imgUrl);
+            startActivity(pageIntent);
+        }
     }
 
     @Override
@@ -138,4 +175,45 @@ public class MainActivity extends ActionBarActivity
         }
     }
 
+    private boolean displayNetworkAlert() {
+        if (!isNetworkConnected() || !isServerConnected()) {
+            if (networkDialog == null) {
+                networkDialog = new AlertDialog.Builder(this).setMessage("Please Check Your Internet Connection and Try Again")
+                        .setTitle("Network Error")
+                        .setCancelable(false)
+                        .setNegativeButton("Retry",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int whichButton) {
+                                        finish();
+                                    }
+                                })
+                        .setPositiveButton("Connect to WIFI",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int whichButton) {
+                                        startActivity(new Intent(Settings.ACTION_WIFI_SETTINGS));
+                                    }
+                                })
+                        .show();
+            } else {
+                networkDialog.show();
+            }
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isServerConnected() {
+        try {
+            InetAddress ipAddr = InetAddress.getByName("triethocduongpho-android.appspot.com");
+            return ipAddr.isReachable(30000);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+
+    private boolean isNetworkConnected() {
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo ni = cm.getActiveNetworkInfo();
+        return null != ni;
+    }
 }
